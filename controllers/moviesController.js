dda76ab58fef3490bd42a16de382daec0902a41d
Jonathanmasa <jonathanmasa97@gmail.com>
2025-03-10 18:12:07 +0100

    feat[env/controller/middlewears]

---
+++ controllers/moviesController.js
@@ -0,0 +1,36 @@
+// importo i dati
+ // const movie = require('../data/films');
+ 
+ // Importiamo il file di connessione al database
+ const connection = require('../data/db');
+ 
+ 
+ // gruppo delle funzione della logica relativa alle rotte dei film
+ 
+ function index(req, res) {
+ 
+
+ }
+ 
+ function show(req, res) {
+ 
+ }
+ 
+ function store(req, res) {
+ 
+ }
+ 
+ function update(req, res) {
+ 
+ }
+ 
+ function modify(req, res) {
+ 
+ };
+ 
+ function destroy(req, res) {
+ 
+ }
+ 
+ // esportiamo tutto
+ module.exports = { index, show, store, update, modify, destroy }
